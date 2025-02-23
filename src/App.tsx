8405e6113a980d4d46d02704e82d9a250496ca00
bannakonz <bannakon10@gmail.com>
2025-02-23 13:36:53 +0700

    chore: add husky, lint-stage, set prettier

--- src/App.tsx
+++ src/App.tsx
@@ -5,13 +5,6 @@ import "./App.css";
 
 function App() {
   const [count, setCount] = useState(0);
-  console.log(reactLogo);
-
-  const a = 10;
-  const b = 20;
-  console.log(a + b);
-
-  console.log(b + b);
 
   return (
     <>
@@ -32,7 +25,7 @@ function App() {
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
-      <p className={"read-the-docs"}>
+      <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
       <h1>test</h1>
